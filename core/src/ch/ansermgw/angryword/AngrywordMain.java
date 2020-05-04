86c2a0653cb6badb8b41980a360fe075666f4994
Ansermoz Gwenaël <gw.ansermoz@gmail.com>
2020-05-04 14:59:17 +0200

    fix render bird

--- core/src/ch/ansermgw/angryword/AngrywordMain.java
+++ core/src/ch/ansermgw/angryword/AngrywordMain.java
@@ -1,8 +1,6 @@
 package ch.ansermgw.angryword;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -13,9 +11,10 @@ public class AngrywordMain extends ApplicationAdapter {
 	Texture img;
 
 	Bird bird;
-	
+
 	@Override
 	public void create () {
+		batch = new SpriteBatch();
 		this.bird = new Bird(0, 0);
 	}
 
@@ -23,9 +22,9 @@ public class AngrywordMain extends ApplicationAdapter {
 	public void render () {
 		batch.begin();
 		bird.draw(batch);
-		batch.begin();
+		batch.end();
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
