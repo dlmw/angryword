f6f233f9c2013408f4facece4fe510372a28b4d5
dlmw <12473240+dlmw@users.noreply.github.com>
2020-11-12 09:35:37 +0100

    Add SemanticWord that throws exception

--- core/src/ch/ansermgw/angryword/models/SemanticWord.java
+++ core/src/ch/ansermgw/angryword/models/SemanticWord.java
@@ -3,6 +3,9 @@ package ch.ansermgw.angryword.models;
 import java.util.HashMap;
 import java.util.Map;
 
+import ch.ansermgw.angryword.exception.TranslationDoesNotExistException;
+import ch.ansermgw.angryword.exception.TranslationExistsException;
+
 public class SemanticWord {
     private final Map<Language, String> values;
 
@@ -10,11 +13,21 @@ public class SemanticWord {
         this.values = new HashMap<>();
     }
 
-    public void addTranslation(Language language, String value) {
+    public void addTranslation(Language language, String value) throws TranslationExistsException {
+        if (this.values.get(language) != null) {
+            throw new TranslationExistsException(language.getDisplayName() + " " + value);
+        }
+
         this.values.put(language, value);
     }
 
-    public String getValue(Language language) {
-        return values.get(language);
+    public String getValue(Language language) throws TranslationDoesNotExistException {
+        String value = values.get(language);
+
+        if (value == null) {
+            throw new TranslationDoesNotExistException(language.getDisplayName());
+        }
+
+        return value;
     }
 }
