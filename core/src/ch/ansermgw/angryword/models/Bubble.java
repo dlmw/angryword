bc1dc8a32b79ca016a66cb9ad7e386d6f6903979
dlmw <12473240+dlmw@users.noreply.github.com>
2020-11-12 09:34:28 +0100

    Update Bubble

--- core/src/ch/ansermgw/angryword/models/Bubble.java
+++ core/src/ch/ansermgw/angryword/models/Bubble.java
@@ -1,14 +1,15 @@
 package ch.ansermgw.angryword.models;
 
-import ch.ansermgw.angryword.resource.WordResource;
 import com.badlogic.gdx.math.Vector2;
+
+import ch.ansermgw.angryword.exception.TranslationDoesNotExistException;
 
 public class Bubble extends TextualObject {
     public static final int WIDTH = 75 * 2;
     public static final int HEIGHT = 65 * 2;
     private static final String SPRITE_NAME = "bubble.png";
 
-    public Bubble(Vector2 position, WordResource word) {
-        super(position, WIDTH, HEIGHT, SPRITE_NAME, word.getTranslation());
+    public Bubble(Vector2 position, SemanticWord word) throws TranslationDoesNotExistException {
+        super(position, WIDTH, HEIGHT, SPRITE_NAME, word.getValue(new Language("Français", "fr"))); // TODO get from current language
     }
 }
