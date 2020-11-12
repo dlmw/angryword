d4d94bfdc26d75ce0c66e6892ffb7bda48e5bc1f
dlmw <12473240+dlmw@users.noreply.github.com>
2020-11-12 09:38:29 +0100

    Fix Panel, forgot to adapt SemanticWord method

--- core/src/ch/ansermgw/angryword/models/Panel.java
+++ core/src/ch/ansermgw/angryword/models/Panel.java
@@ -2,14 +2,16 @@ package ch.ansermgw.angryword.models;
 
 import com.badlogic.gdx.math.Vector2;
 
+import ch.ansermgw.angryword.exception.TranslationDoesNotExistException;
+
 public class Panel extends TextualObject {
     public static final int WIDTH = 510 / 2;
     public static final int HEIGHT = 486 / 2;
     private static final String SPRITE_NAME = "panel.png";
     private SemanticWord semanticWord;
 
-    public Panel(Vector2 position, SemanticWord word) {
-        super(position, WIDTH, HEIGHT, SPRITE_NAME, word.getWord(), new Vector2(0, -Math.abs(HEIGHT / 4)));
+    public Panel(Vector2 position, SemanticWord word) throws TranslationDoesNotExistException {
+        super(position, WIDTH, HEIGHT, SPRITE_NAME, word.getValue(new Language("Français", "fr")), new Vector2(0, -Math.abs(HEIGHT / 4))); // TODO get from current language
         semanticWord = word;
     }
 
@@ -17,7 +19,7 @@ public class Panel extends TextualObject {
         return semanticWord;
     }
 
-    public void setWordResource(SemanticWord wordResource) {
+    public void setWordResource(SemanticWord wordResource) throws TranslationDoesNotExistException {
         this.semanticWord = wordResource;
         this.glyphLayout.setText(font, semanticWord.getValue(new Language("Français", "fr"))); // TODO get from current language
     }
