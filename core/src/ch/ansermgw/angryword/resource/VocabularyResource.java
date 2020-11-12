14470cc933c477a491351e3ec10ad91207da6790
dlmw <12473240+dlmw@users.noreply.github.com>
2020-11-12 09:22:47 +0100

    Modify VocabularyResource to use SemanticWord instead of Word

--- core/src/ch/ansermgw/angryword/resource/VocabularyResource.java
+++ core/src/ch/ansermgw/angryword/resource/VocabularyResource.java
@@ -1,15 +1,16 @@
 package ch.ansermgw.angryword.resource;
 
 import ch.ansermgw.angryword.activities.Play;
+import ch.ansermgw.angryword.models.SemanticWord;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class VocabularyResource {
-    private final List<WordResource> words;
+    private final List<SemanticWord> words;
     private final String name;
 
-    public VocabularyResource(ArrayList<WordResource> words, String name) {
+    public VocabularyResource(ArrayList<SemanticWord> words, String name) {
         this.words = words;
         this.name = name;
     }
@@ -19,39 +20,7 @@ public class VocabularyResource {
         this.name = name;
     }
 
-    public void addWord(WordResource word) {
-        words.add(word);
-    }
-
-    public WordResource getRandomUnusedWordResource() {
-        List<WordResource> unusedWord = new ArrayList<>();
-
-        for (WordResource word : words) {
-            if (!word.isUsed()) {
-                unusedWord.add(word);
-            }
-        }
-
-        return getRandomWordResourceFromList(unusedWord);
-    }
-
-    public WordResource getRandomUsedWordResource() {
-        List<WordResource> usedWord = new ArrayList<>();
-
-        for (WordResource word : words) {
-            if (word.isUsed()) {
-                usedWord.add(word);
-            }
-        }
-
-        return getRandomWordResourceFromList(usedWord);
-    }
-
-    private WordResource getRandomWordResourceFromList(List<WordResource> wordResources) {
-        if (wordResources.size() < 1) {
-            return null;
-        }
-
-        return wordResources.get(Play.rand.nextInt(wordResources.size()));
+    public void addSemanticWord(SemanticWord sw) {
+        words.add(sw);
     }
 }
